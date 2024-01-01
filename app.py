7130610785c6cf452b18462687748a5db02c96af
Zhijun Gao <galty687@gmail.com>
2024-01-01 11:16:12 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # 加载 .env 文件
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+
+st.title('术语提取')
+st.write('这是一个文本提取系统，基于ChatGLM开发')
+
+prompt = st.text_input("请输入术语提取提示词：", value="请提取术语：")
+
+input_text=st.text_area("请输入需要提取的文本，注意长度需要少于500字：")
+
+combined_input = f"{prompt}\n\n{input_text}"
+
+
+
+def process_with_openai(text):
+    try:
+        # 使用 OpenAI 聊天模型进行处理
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",  # 模型名称
+            messages=[
+                {"role": "user", "content": text}
+            ],
+            max_tokens=8000  # 设置最大令牌数
+        )
+        return response.choices[0].message['content'].strip()
+    except Exception as e:
+        return str(e)
+
+if st.button('提取术语'):
+    with st.spinner('正在处理中，请稍候...'):
+
+
+
+        if input_text:
+            result = process_with_openai(combined_input)
+            st.write(result)
+        else:
+            st.write("请输入一些文本。")
+
+    st.success('处理完成！')    
